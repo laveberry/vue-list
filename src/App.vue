888a0c2f579cb07e9f90dfb0c4c935d1fd490f45
laveberry <laveberry7@gmail.com>
2021-08-19 09:57:59 +0900

    git instagram 초기세팅

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p v-if="isMonday">{{안녕}}</p>
+    <p v-for="이름 in 이름들" v-bind:key="이름">{{이름}}</p>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      이름들 : ['아이유','태연','수지','서강준','이진욱'],
+      안녕 : '안녕하세요',
+      isMonday : false
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
